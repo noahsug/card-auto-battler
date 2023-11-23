a6ca7d9405ff3758550881e944d79584273f7000
noah.sugarman <noah.sugarman@airbnb.com>
2023-11-23 01:03:34 -0800

    take turn every 2s

--- src/components/BattleScreen.tsx
+++ src/components/BattleScreen.tsx
@@ -2,22 +2,22 @@ import { useEffect } from 'react';
 
 import './BattleScreen.css';
 
-import { useGame, useGameDispatch } from './GameContext';
+import { useGame, useActions } from './GameContext';
 import Player from './Player';
 
 export default function BattleScreen() {
   const { turn } = useGame();
-  const dispatch = useGameDispatch();
+  const { nextTurn } = useActions();
 
   // take turns every 2 seconds
   useEffect(() => {
-    const nextTurn = () => dispatch({ type: 'NEXT_TURN' });
     const timeout = setTimeout(nextTurn, 2000);
     return () => clearInterval(timeout);
-  }, [dispatch, turn]);
+  }, [nextTurn, turn]);
 
   return (
     <div className="BattleScreen">
+      Turn: {turn}
       <Player isOpponent={true} />
       <div className="BattleScreen-divider" />
       <Player isOpponent={false} />
