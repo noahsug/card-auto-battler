9996a7a65a4db5e3f8d99e789c55731fb0b1e10b
noah.sugarman <noah.sugarman@airbnb.com>
2024-09-07 00:34:13 -0700

    button story

--- src/ui/components/shared/Button.tsx
+++ src/ui/components/shared/Button.tsx
@@ -1,11 +1,14 @@
 import { styled } from 'styled-components';
 
 export default styled.button`
-  font-size: 2rem;
-  padding: 0.4em 1em 0.4em;
-  border: none;
-  border-radius: 0.3em;
-  background: darkgray;
-  color: black;
+  font-size: 3rem;
   margin: 0.5em auto;
+  padding: 0.5rem 1rem;
+  background: transparent;
+  border-top-left-radius: 255px 15px;
+  border-top-right-radius: 15px 225px;
+  border-bottom-right-radius: 225px 15px;
+  border-bottom-left-radius: 15px 255px;
+  font-family: 'Amatic';
+  letter-spacing: 0.1em;
 `;
