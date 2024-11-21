9fab6ec7d24ac25228b8fa9b3680797079e70c10
noah.sugarman <noah.sugarman@airbnb.com>
2024-11-20 19:42:23 -0800

    fix double animation issue - useChain is broken

--- src/ui/components/BattleResultOverlay/BattleResultOverlay.tsx
+++ src/ui/components/BattleResultOverlay/BattleResultOverlay.tsx
@@ -1,22 +1,22 @@
+import { animated, config, useSpring } from '@react-spring/web';
 import { styled } from 'styled-components';
-import { useSpring, animated, useChain, config, useSpringRef } from '@react-spring/web';
 
 import livesImage from '../../images/hourglass.png';
-import battleImage from '../../images/swords.png';
 import skullImage from '../../images/skull.png';
-import brokenSkullImage from './broken-skull.png';
+import battleImage from '../../images/swords.png';
 import rewindImage from './backward-time.png';
+import brokenSkullImage from './broken-skull.png';
 
+import { useState } from 'react';
 import { MAX_LOSSES, MAX_WINS } from '../../../game/constants';
 import { GameState } from '../../../game/gameState';
-import { Button } from '../shared/Button';
-import { Container } from '../shared/Container';
+import { getIsBossBattle, getIsGameOver } from '../../../game/utils/selectors';
+import { plural } from '../../../utils/plural';
 import { useUnits } from '../../hooks/useUnits';
 import { maskImage } from '../../style';
+import { Button } from '../shared/Button';
+import { Container } from '../shared/Container';
 import { Row } from '../shared/Row';
-import { plural } from '../../../utils/plural';
-import { useState } from 'react';
-import { getIsBossBattle, getIsGameOver } from '../../../game/utils/selectors';
 
 const Header = styled(animated.h2)`
   text-align: center;
@@ -86,59 +86,51 @@ export function BattleResultOverlay({ game, onContinue, wonLastBattle }: Props) 
   const isGameOver = getIsGameOver(game);
   const isBossBattle = getIsBossBattle(game);
 
+  const colorToAnimateTo = wonLastBattle ? 'hsl(150, 50%, 50%)' : 'hsl(0, 50%, 50%)';
+  const [message, continueText] = getBattleResultMessage({ wonLastBattle, wins, losses });
+  const offsetAnimationDelay = 500;
+
   // used to animate the value changing from its previous value to its current value
   const [offset, setOffset] = useState(1);
-  const displayedLosses = losses - (wonLastBattle ? 0 : offset);
+  const displayedLivesLeft = MAX_LOSSES - losses + (wonLastBattle ? 0 : offset);
   const displayedWins = wins - (wonLastBattle ? offset : 0);
-
-  const livesLeft = MAX_LOSSES - displayedLosses;
-  const battleNumber = displayedWins;
 
   const [showBrokenSkull, setShowBrokenSkull] = useState(false);
   const skullImageToDisplay = showBrokenSkull ? brokenSkullImage : skullImage;
   const winsImage = wins >= MAX_WINS ? skullImageToDisplay : battleImage;
 
-  const colorToAnimateTo = wonLastBattle ? 'hsl(150, 50%, 50%)' : 'hsl(0, 50%, 50%)';
-
-  const [message, continueText] = getBattleResultMessage({ wonLastBattle, wins, losses });
-
-  const headerAnimation = useSpringRef();
-  const headerStyle = useSpring({
+  const [headerStyle] = useSpring(() => ({
     from: { y: u(50) },
-    to: [{ y: 0, config: config.wobbly }],
-    ref: headerAnimation,
-  });
+    to: { y: 0, config: config.wobbly },
+  }));
 
-  // TODO: fix the animation playing twice
-  const labelAnimation = useSpringRef();
-  const labelStyle = useSpring({
+  const [labelStyle] = useSpring(() => ({
     from: { color: '#fcfafb', scale: 1 },
     to: [
       { color: colorToAnimateTo, scale: 1.3 },
       ...(isGameOver ? [] : [{ color: '#fcfafb', scale: 1 }]),
     ],
+    delay: offsetAnimationDelay,
     onStart: () => {
       setOffset(0);
     },
     onRest: () => {
       setShowBrokenSkull(true);
     },
-    ref: labelAnimation,
-  });
+  }));
 
-  const iconStyle = useSpring({
+  const [iconStyle] = useSpring(() => ({
     from: { backgroundColor: '#fcfafb' },
     to: [
       { backgroundColor: colorToAnimateTo },
       ...(isGameOver ? [] : [{ backgroundColor: '#fcfafb' }]),
     ],
-    ref: labelAnimation,
-  });
+    delay: offsetAnimationDelay,
+  }));
 
+  // animate either the number of wins or the number of lives left, depending on if the user won or lost
   const [winsStyle, livesStyle] = wonLastBattle ? [labelStyle, {}] : [{}, labelStyle];
   const [winsIconStyle, livesIconStyle] = wonLastBattle ? [iconStyle, {}] : [{}, iconStyle];
-
-  useChain([headerAnimation, labelAnimation]);
 
   return (
     <Container>
@@ -146,12 +138,12 @@ export function BattleResultOverlay({ game, onContinue, wonLastBattle }: Props) 
       <ProgressRow>
         <Label style={winsStyle}>
           <Icon style={winsIconStyle} src={winsImage} />
-          <span>{`wins ${battleNumber}/${MAX_WINS}`}</span>
+          <span>{`wins ${displayedWins}/${MAX_WINS}`}</span>
         </Label>
         <Label style={livesStyle}>
           <Icon style={livesIconStyle} src={livesImage} />
           <span>
-            {livesLeft} {plural(livesLeft, 'rewind')}
+            {displayedLivesLeft} {plural(displayedLivesLeft, 'rewind')}
           </span>
         </Label>
       </ProgressRow>
